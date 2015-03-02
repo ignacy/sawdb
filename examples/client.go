7d6e8c15f444a90d673fe6a826e7fa86c058355d
Ignacy Moryc <ignacy.moryc@up-next.com>
2015-03-02 19:18:11 +0100

    Handle actions over tcp socket

---
+++ examples/client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net"
+)
+
+const Host = "127.0.0.1:9000"
+
+func main() {
+  conn, err := net.Dial("tcp", Host)
+  if err != nil {
+    fmt.Println("Could not connect to sawDB")
+  }
+
+  keyToStore := []byte("STORE\tkey\tvalue\t\r\n")
+  conn.Write(keyToStore)
+
+  messageBuffer := make([]byte, 1024)
+  _, err = conn.Read(messageBuffer)
+  if err != nil {
+    log.Println("Failed to read in a message")
+  }
+
+  response := string(messageBuffer)
+  log.Println("Received response was: ", response)
+}
--- saw.go
+++ saw.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+  "errors"
   "log"
   "net"
+  "strings"
+
+  "github.com/ignacy/sawdb/api"
 )
 
 type ConnectionManager struct {
   initiated bool
+  db        *api.SawDB
 }
 
 func Initiate() *ConnectionManager {
   return &ConnectionManager{
     initiated: false,
+    db:        api.NewSawDB(),
   }
+}
+
+func handleAction(action string, cM *ConnectionManager) error {
+  components := strings.Split(action, "\t")
+  if len(components) < 3 {
+    return errors.New("Malformed action description")
+  }
+
+  if components[0] == "STORE" {
+    cM.db.Put(components[1], components[2])
+  }
+
+  value, err := cM.db.Get(components[1])
+  if err != nil {
+    log.Println("Failed to store record")
+  } else {
+    log.Println("Stored record: ", value)
+  }
+  return nil
 }
 
 func (cM *ConnectionManager) Listen(listener net.Listener) {
@@ -25,6 +50,21 @@ func (cM *ConnectionManager) Listen(listener net.Listener) {
     }
 
     log.Println(conn.RemoteAddr(), " connected")
+
+    messageBuffer := make([]byte, 1024)
+    _, err = conn.Read(messageBuffer)
+    if err != nil {
+      log.Println("Failed to read in a message")
+    }
+
+    message := string(messageBuffer)
+    log.Println("Received was: ", message)
+
+    if err = handleAction(message, cM); err != nil {
+      log.Println("Error while handling action ", err)
+    }
+
+    conn.Write([]byte("All good action handled \t\r\n"))
   }
 }
 
