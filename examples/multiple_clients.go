cde98c44a0ffe96b3cfdf345566c686198ce9a5d
Ignacy Moryc <ignacy.moryc@up-next.com>
2015-04-22 15:29:43 +0200

    Add example with 2 clients

---
+++ examples/multiple_clients.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net"
+)
+
+const Host = "127.0.0.1:9000"
+
+func main() {
+  conn1, err := net.Dial("tcp", Host)
+  if err != nil {
+    fmt.Println("Could not connect to sawDB")
+  }
+
+  defer conn1.Close()
+
+  conn2, err := net.Dial("tcp", Host)
+  if err != nil {
+    fmt.Println("Could not connect to sawDB")
+  }
+
+  defer conn2.Close()
+
+  keyToStore := []byte("S\tkey\tvalue\t")
+  go conn1.Write(keyToStore)
+
+  messageBuffer := make([]byte, 1024)
+  _, err = conn2.Read(messageBuffer)
+  if err != nil {
+    log.Println("Failed to read in a message")
+  }
+
+  response := string(messageBuffer)
+  log.Println("Received response was: ", response)
+
+  getValue := []byte("G\tvalue\t\r\n")
+  go conn2.Write(getValue)
+
+  messageBuffer = make([]byte, 1024)
+  _, err = conn1.Read(messageBuffer)
+  if err != nil {
+    log.Println("Failed to read in a message")
+  }
+
+  response = string(messageBuffer)
+  log.Println("Received response was: ", response)
+}
